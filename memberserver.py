07c56d7af3572af1c5210a72aba2068c261cd632
Roisin-Fallon <48020027+Roisin-Fallon@users.noreply.github.com>
2020-01-07 11:42:23 +0000

    Added cors

--- memberserver.py
+++ memberserver.py
@@ -1,7 +1,10 @@
 from flask import Flask, jsonify, abort, render_template, request, redirect, url_for, session,flash
 from memberDAO import memberDAO
+from flask_cors import CORS
 
 app = Flask(__name__, static_url_path='', static_folder='.', template_folder="")
+CORS(app)
+
 app.secret_key = 'super secret key'
 
 @app.route("/", methods=['GET', 'POST'])
